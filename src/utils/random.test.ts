e90e3ebc30ac32b0aeb76e50c5262f3518afaf4c
Vladislav Yakovlev <vlad309523@gmail.com>
2022-07-10 16:22:02 +0400

    Add tests for utils/random

---
+++ src/utils/random.test.ts
@@ -0,0 +1,29 @@
+import { getRandomItem, shuffleItems } from './random';
+
+beforeEach(() => {
+  jest.spyOn(global.Math, 'random').mockReturnValue(0.3);
+});
+
+afterEach(() => {
+  jest.spyOn(global.Math, 'random').mockRestore();
+});
+
+describe('getRandomItem', () => {
+  test('should return value', () => {
+    expect(getRandomItem(['a', 'b', 'c', 'd'])).toBe('b');
+  });
+
+  test('should return firstItem when only one is presented', () => {
+    expect(getRandomItem(['a'])).toBe('a');
+  });
+});
+
+describe('shuffleItems', () => {
+  test('should shuffle array', () => {
+    expect(shuffleItems(['a', 'b', 'c', 'd'])).toStrictEqual(['d', 'c', 'a', 'b']);
+  });
+
+  test('should accept empty array', () => {
+    expect(shuffleItems([])).toStrictEqual([]);
+  });
+});
