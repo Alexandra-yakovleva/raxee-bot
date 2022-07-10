0af75e3b07064ad1518988229426f6850cc3f9c1
Vladislav Yakovlev <vlad309523@gmail.com>
2022-07-10 13:18:14 +0400

    Fix startsWith

--- src/utils/remeda/startsWith.ts
+++ src/utils/remeda/startsWith.ts
@@ -1,8 +1,8 @@
 // eslint-disable-next-line no-restricted-imports
 import { purry } from 'remeda';
 
-export function startsWith(str: string, searchString: string, position?: number): boolean;
-export function startsWith(str: string, position?: number): (searchString: string) => boolean;
+export function startsWith(str: string, search: string): boolean;
+export function startsWith(search: string): (str: string) => boolean;
 
 export function startsWith() {
   // eslint-disable-next-line @typescript-eslint/no-use-before-define, prefer-rest-params
@@ -10,4 +10,4 @@ export function startsWith() {
 }
 
 // eslint-disable-next-line @typescript-eslint/naming-convention, no-underscore-dangle
-const _startsWith = (str: string, searchString: string, position?: number) => str.startsWith(searchString, position);
+const _startsWith = (str: string, search: string) => str.startsWith(search);
