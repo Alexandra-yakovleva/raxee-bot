597b99a99b98f21e8a8048219233d2f29339877b
Vladislav Yakovlev <vlad309523@gmail.com>
2022-07-11 16:32:19 +0400

    Add tests for utils/getPokerHighHand

---
+++ src/utils/getPokerHighHand.test.ts
@@ -0,0 +1,18 @@
+import { PokerCard } from '../classes/PokerCard';
+import { PokerCombination } from '../classes/PokerCombination';
+
+import { getPokerHighHand } from './getPokerHighHand';
+
+test('should return combination for every card', () => {
+  expect(getPokerHighHand([
+    new PokerCard(3, 4),
+    new PokerCard(2, 3),
+    new PokerCard(1, 2),
+    new PokerCard(0, 1),
+  ])).toStrictEqual([
+    new PokerCombination(0, [new PokerCard(3, 4)]),
+    new PokerCombination(0, [new PokerCard(2, 3)]),
+    new PokerCombination(0, [new PokerCard(1, 2)]),
+    new PokerCombination(0, [new PokerCard(0, 1)]),
+  ]);
+});
