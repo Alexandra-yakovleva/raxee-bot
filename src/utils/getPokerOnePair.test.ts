5fcd4f652424093c671519138aa15edc9077d058
Vladislav Yakovlev <vlad309523@gmail.com>
2022-07-11 16:48:11 +0400

    Add tests for utils/getPokerTwoPairs

--- src/utils/getPokerOnePair.test.ts
+++ src/utils/getPokerOnePair.test.ts
@@ -9,6 +9,7 @@ test('should return all available pairs', () => {
     new PokerCard(2, 2),
     new PokerCard(1, 1),
     new PokerCard(0, 1),
+    new PokerCard(3, 9),
   ])).toStrictEqual([
     new PokerCombination(1, [new PokerCard(3, 2), new PokerCard(2, 2)]),
     new PokerCombination(1, [new PokerCard(1, 1), new PokerCard(0, 1)]),
@@ -19,6 +20,7 @@ test('should return all available pairs', () => {
     new PokerCard(2, 2),
     new PokerCard(1, 2),
     new PokerCard(0, 1),
+    new PokerCard(3, 9),
   ])).toStrictEqual([
     new PokerCombination(1, [new PokerCard(3, 2), new PokerCard(2, 2)]),
     new PokerCombination(1, [new PokerCard(3, 2), new PokerCard(1, 2)]),
@@ -30,6 +32,7 @@ test('should return all available pairs', () => {
     new PokerCard(2, 2),
     new PokerCard(1, 2),
     new PokerCard(0, 2),
+    new PokerCard(3, 9),
   ])).toStrictEqual([
     new PokerCombination(1, [new PokerCard(3, 2), new PokerCard(2, 2)]),
     new PokerCombination(1, [new PokerCard(3, 2), new PokerCard(1, 2)]),
@@ -44,6 +47,7 @@ test('should return all available pairs', () => {
     new PokerCard(2, 2),
     new PokerCard(1, 3),
     new PokerCard(0, 1),
+    new PokerCard(3, 9),
   ])).toStrictEqual([
     new PokerCombination(1, [new PokerCard(3, 2), new PokerCard(2, 2)]),
   ]);
@@ -55,5 +59,6 @@ test('should return nothing when no pairs', () => {
     new PokerCard(2, 3),
     new PokerCard(1, 2),
     new PokerCard(0, 1),
+    new PokerCard(3, 9),
   ])).toStrictEqual([]);
 });
