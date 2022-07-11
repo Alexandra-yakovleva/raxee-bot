ac703dbfb03bd3beba5d6a4503657904e39fb225
Vladislav Yakovlev <vlad309523@gmail.com>
2022-07-11 15:40:38 +0400

    Add tests for classes/PokerCombination

--- src/classes/PokerCombination.test.ts
+++ src/classes/PokerCombination.test.ts
@@ -39,16 +39,24 @@ describe('#secondCard', () => {
 });
 
 describe('#weight', () => {
-  test('should return correct weight', () => {
+  test('should return weight', () => {
     const cards = [new PokerCard(1, 3), new PokerCard(0, 7), new PokerCard(3, 4)];
     const combination = new PokerCombination(3, cards);
     expect(combination.weight).toBe(30300);
   });
 
-  test('should return correct weight when secondCard is presented', () => {
+  test('should return weight when secondCard is presented', () => {
     const cards = [new PokerCard(1, 3), new PokerCard(0, 7), new PokerCard(3, 4), new PokerCard(3, 5)];
     const combination = new PokerCombination(2, cards);
     expect(combination.weight).toBe(20304);
+  });
+});
+
+describe('#levelName', () => {
+  test('should return level name', () => {
+    const cards = [new PokerCard(1, 3), new PokerCard(0, 7), new PokerCard(3, 4)];
+    const combination = new PokerCombination(3, cards);
+    expect(combination.levelName).toBe('тройка');
   });
 });
 
